f6c45b44724efa623c9073580755fc7878fcdf5f
ysf <yuansssf@gmail.com>
2019-08-21 17:44:12 +0800

    监听适配器内部控件点击事件

--- app/src/main/java/com/example/yuan/todo/TodoAdapter.java
+++ app/src/main/java/com/example/yuan/todo/TodoAdapter.java
@@ -1,10 +1,15 @@
 package com.example.yuan.todo;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,18 +19,23 @@ import butterknife.ButterKnife;
 
 public class TodoAdapter extends ArrayAdapter<Todo> {
 
+    private final String TAG = this.getContext().getClass().getSimpleName();
+
     int resourceId;
+    private List<Todo> todoList;
 
     public TodoAdapter( Context context, int resource, List<Todo> objects) {
         super(context, resource, objects);
         resourceId = resource;
+        todoList = objects;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Todo todo = getItem(position);
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Log.d(TAG, "getView: " + position);
+        final Todo todo = getItem(position);
         View view;
-        ViewHolder viewHolder;
+        final ViewHolder viewHolder;
         if (convertView != null) {
             view = convertView;
             viewHolder = (ViewHolder) view.getTag();
@@ -35,9 +45,49 @@ public class TodoAdapter extends ArrayAdapter<Todo> {
             view.setTag(viewHolder);
         }
         char[] todoTitle = todo.getTodo().toCharArray();
-        viewHolder.todo.setText(todoTitle[0]);
+        Log.d(TAG, "getView: " + todoTitle[0]);
+        viewHolder.todo.setText(String.valueOf(todoTitle[0]));
         viewHolder.date.setText(todo.getDate());
         viewHolder.time.setText(todo.getTime());
+
+        viewHolder.todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: " + getItem(position).getTodo());
+                ToastUtil.showShort(getContext(), getItem(position).getTodo());
+            }
+        });
+
+        viewHolder.deleteItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: " + "删除开始");
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("是否删除该待办时间");
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        todoList.remove(position);
+                        notifyDataSetChanged();
+                        DatabaseHelper databaseHelper = new DatabaseHelper(getContext(), "TODO", null, 1);
+                        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+                        //执行多条件sql语句进行删除具体数据项
+                        String sql = "delete from todo where title = '" + todo.getTodo() +"' and date = '" + todo.getDate() + "' and time = '" + todo.getTime() + "' and code = '" + todo.getCode() + "'";
+                        sqLiteDatabase.execSQL(sql);
+                        sqLiteDatabase.close();
+                        Log.d(TAG, "onClick: " + "删除成功");
+                    }
+                });
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
+
         return view;
     }
 
@@ -49,6 +99,8 @@ public class TodoAdapter extends ArrayAdapter<Todo> {
         TextView date;
         @BindView(R.id.timeText)
         TextView time;
+        @BindView(R.id.deleteItem)
+        ImageView deleteItem;
 
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
