53da88b508c7109eacab08ae048472b2db0fb413
ysf <yuansssf@gmail.com>
2019-08-25 16:58:28 +0800

    接受历史上的今天的数据

---
+++ app/src/main/java/com/example/yuan/todo/bean/Bean.java
@@ -0,0 +1,13 @@
+package com.example.yuan.todo.bean;
+
+import java.util.List;
+
+/**
+ * Created by jarvis yuen
+ * Email: yuansssf@gmail.com
+ */
+public class Bean {
+    int error_code;
+    String reason;
+    List<Result> resultList;
+}
---
+++ app/src/main/java/com/example/yuan/todo/bean/Result.java
@@ -0,0 +1,16 @@
+package com.example.yuan.todo.bean;
+
+/**
+ * Created by jarvis yuen
+ * Email: yuansssf@gmail.com
+ */
+public class Result {
+    int day;
+    String des;
+    int id;
+    String lunar;
+    int month;
+    String pic;
+    String title;
+    int year;
+}
