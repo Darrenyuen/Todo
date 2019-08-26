8d1a72534edb8a512a4c7fc57db39560cf89c4dd
ysf <yuansssf@gmail.com>
2019-08-26 14:48:38 +0800

    封装数据请求类

---
+++ app/src/main/java/com/example/yuan/todo/util/DataAPI.java
@@ -0,0 +1,23 @@
+package com.example.yuan.todo.util;
+
+import com.example.yuan.todo.bean.Bean;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Created by jarvis yuen
+ * Email: yuansssf@gmail.com
+ */
+public interface DataAPI {
+    /**
+     *  http://api.juheapi.com/japi/toh?key=48d0bc89de1ea3be90e78aa9f1d79a12&v=1.0&month=8&day=25
+     *  key : 48d0bc89de1ea3be90e78aa9f1d79a12
+     *  v : 1.0
+     *  month
+     *  day
+     */
+    @GET("key=8d0bc89de1ea3be90e78aa9f1d79a12&v=1.0")
+    Observable<Bean> getData(@Query("month") int month, @Query("day") int day);
+}
---
+++ app/src/main/java/com/example/yuan/todo/util/HttpMethods.java
@@ -0,0 +1,67 @@
+package com.example.yuan.todo.util;
+
+import com.example.yuan.todo.bean.Bean;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.observers.Subscribers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by jarvis yuen
+ * Email: yuansssf@gmail.com
+ */
+
+/**
+ * 单例模式实现
+ */
+public class HttpMethods {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    private final String BASE_URL = "http://api.juheapi.com/japi/toh";
+    private final int DEFAULT_TIMEOUT = 5;
+
+    private HttpMethods httpMethods;
+    private Retrofit retrofit;
+    private DataAPI dataAPI;
+
+    public HttpMethods() {
+        //手动创建一个OkHttpClient并设置超时时间
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+
+        retrofit = new Retrofit.Builder()
+                .client(httpClientBuilder.build())
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(BASE_URL)
+                .build();
+
+        dataAPI = retrofit.create(DataAPI.class);
+    }
+
+    //在访问HttpMethods时创建单例
+    private static class SingletonHolder{
+        private static final HttpMethods INSTANCE = new HttpMethods();
+    }
+
+    //获取单例
+    public static HttpMethods getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+
+    public void getDate(Subscriber<Bean> subscriber, int month, int day) {
+        dataAPI.getData(month, day)
+                .subscribeOn(Schedulers.io())   //指定subscribe()发生在IO线程
+                .unsubscribeOn(Schedulers.io()) //避免内存泄露
+                .observeOn(AndroidSchedulers.mainThread())  //指定Subscriber的回调发生在主线程
+                .subscribe(subscriber);
+    }
+}
